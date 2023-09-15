49c721af21aac44141d6cdbdc7163c703fdc7eb2
B_Nurmagambetov <b_nurmagambetov@kbtu.kz>
2023-09-15 13:14:00 +0600

    Leetcode

---
+++ 1 assignment/2666.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let isCalled = false;
+    let res;
+    return function(...args){
+        if(!isCalled){
+            result = fn(...args);
+            isCalled = true;
+            return result
+        }else{
+            return undefined;
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
